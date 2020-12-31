35119c448af4ee50a740ee402a55c7d42913c3e9
davidestroveglia <72864974+davidestroveglia@users.noreply.github.com>
2020-12-31 17:13:45 +0100

    costruttore login

--- src/logic/control/ControllerLogin.java
+++ src/logic/control/ControllerLogin.java
@@ -1,5 +1,19 @@
 package logic.control;
 
 public class ControllerLogin {
+	//*****PROVA SINGLETON*****
+	private static ControllerLogin INSTANCE = null;
+	
+	//*****COSTRUTTORE*****
+	private ControllerLogin() {} //"private" perché tramite getInstance() potrà essere chiamato solo dall'interno
+	
+	public static ControllerLogin getInstance() {
+		if(INSTANCE==null)
+			INSTANCE=new ControllerLogin();
+		return INSTANCE;
+	}
+	//*****FINE SINGLETON*****
+	
+	//*****METODI*****
 
 }
