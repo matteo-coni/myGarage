6030a3a429f9d8ad60ca6dcd765338c6f9009531
davidestroveglia <72864974+davidestroveglia@users.noreply.github.com>
2020-12-31 14:26:16 +0100

    aggiunta di entità e controllori principali + alcuni attributi e metodi

--- src/logic/model/Mechanic.java
+++ src/logic/model/Mechanic.java
@@ -1,10 +1,51 @@
 package logic.model;
 
 public class Mechanic {
-	
+	//*****ATTRIBUTI*****
 	private String name;
+	private String surname;
+	private String garage;
+	private String address;
+	private String zone;
+	private String city;
 	
-	public Mechanic(String name) {
+	//*****METODI*****
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
 		this.name = name;
 	}
+	public String getSurname() {
+		return surname;
+	}
+	public void setSurname(String surname) {
+		this.surname = surname;
+	}
+	public String getGarage() {
+		return garage;
+	}
+	public void setGarage(String garage) {
+		this.garage = garage;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getZone() {
+		return zone;
+	}
+	public void setZone(String zone) {
+		this.zone = zone;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	
+		
 }
--- src/logic/model/Vehicle.java
+++ src/logic/model/Vehicle.java
@@ -1,5 +1,86 @@
 package logic.model;
 
 public class Vehicle {
-
+	//*****ATTRIBUTI*****
+	private String licensePlate;
+	private String vehicleBrand;
+	private String vehicleModel;
+	private int vehicleDisplacement;
+	private int vehiclePowertrains;
+	private String vehicleFuel;
+	private Date vehicleInsurance;
+	private Date vehicleTax;
+	private Date vehicleReview;
+	private Date vehicleService;
+	private Image img;
+	
+	//*****METODI*****
+	public String getLicensePlate() {
+		return licensePlate;
+	}
+	public void setLicensePlate(String licensePlate) {
+		this.licensePlate = licensePlate;
+	}
+	public String getVehicleBrand() {
+		return vehicleBrand;
+	}
+	public void setVehicleBrand(String vehicleBrand) {
+		this.vehicleBrand = vehicleBrand;
+	}
+	public String getVehicleModel() {
+		return vehicleModel;
+	}
+	public void setVehicleModel(String vehicleModel) {
+		this.vehicleModel = vehicleModel;
+	}
+	public int getVehicleDisplacement() {
+		return vehicleDisplacement;
+	}
+	public void setVehicleDisplacement(int vehicleDisplacement) {
+		this.vehicleDisplacement = vehicleDisplacement;
+	}
+	public int getVehiclePowertrains() {
+		return vehiclePowertrains;
+	}
+	public void setVehiclePowertrains(int vehiclePowertrains) {
+		this.vehiclePowertrains = vehiclePowertrains;
+	}
+	public String getVehicleFuel() {
+		return vehicleFuel;
+	}
+	public void setVehicleFuel(String vehicleFuel) {
+		this.vehicleFuel = vehicleFuel;
+	}
+	public Date getVehicleInsurance() {
+		return vehicleInsurance;
+	}
+	public void setVehicleInsurance(Date vehicleInsurance) {
+		this.vehicleInsurance = vehicleInsurance;
+	}
+	public Date getVehicleTax() {
+		return vehicleTax;
+	}
+	public void setVehicleTax(Date vehicleTax) {
+		this.vehicleTax = vehicleTax;
+	}
+	public Date getVehicleReview() {
+		return vehicleReview;
+	}
+	public void setVehicleReview(Date vehicleReview) {
+		this.vehicleReview = vehicleReview;
+	}
+	public Date getVehicleService() {
+		return vehicleService;
+	}
+	public void setVehicleService(Date vehicleService) {
+		this.vehicleService = vehicleService;
+	}
+	public Image getImg() {
+		return img;
+	}
+	public void setImg(Image img) {
+		this.img = img;
+	}
+	
+	
 }
