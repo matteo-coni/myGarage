365d5237786919223ffbc3bed128d505706384c1
Matteo Coni <57217715+matteo-coni@users.noreply.github.com>
2021-01-04 23:39:32 +0100

    main

--- src/logic/view/desktop/Main.java
+++ src/logic/view/desktop/Main.java
@@ -1,23 +1,22 @@
 package logic.view.desktop;
 
-
 import javafx.application.Application;
-import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
-import javafx.fxml.*;
+import javafx.stage.Stage;
 
-public class MainDesktop extends Application {
-	
+
+/*public class Main {
+
+}*/
+
+public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
 			BorderPane root = new BorderPane();
 			Scene scene = new Scene(root,400,400);
-			
-			root = FXMLLoader.load(getClass().getResource("ViewDesktop.fxml"));
-			
-			//scene.getStylesheets().add(getClass().getResource("ViewDesktop.fxml").toExternalForm());
+			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
 		} catch(Exception e) {
