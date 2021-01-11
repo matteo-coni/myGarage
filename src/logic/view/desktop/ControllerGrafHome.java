588ea7a94fe8093af94230e641937259cbd3ad98
Matteo Coni <57217715+matteo-coni@users.noreply.github.com>
2021-01-12 00:09:57 +0100

    view

--- src/logic/view/desktop/ControllerGrafHome.java
+++ src/logic/view/desktop/ControllerGrafHome.java
@@ -8,8 +8,10 @@ import javafx.scene.*;
 import javafx.scene.image.*;
 import javafx.scene.layout.*;
 import javafx.fxml.FXML;
+import javafx.scene.shape.*;
+import javafx.scene.paint.Color;
 
-//import logic.view.desktop.Main;
+import logic.view.desktop.ViewOwnerSaveVehicle;
 
 
 public class ControllerGrafHome /* extends Application*/ {
@@ -19,13 +21,24 @@ public class ControllerGrafHome /* extends Application*/ {
 	Button button1;
 	Button button2;
 	Button button3;
-	Button button4;
 	Image imageVehicle;
+	GridPane grid;
+	
+	@FXML
+	AnchorPane anchor;
+	
+	//ViewOwnerSaveVehicle viewVehicle = new ViewOwnerSaveVehicle();
 	
 	@FXML
 	public void exit() {
 		//button.setText("oh");
 		System.out.println("ciaociao");
+		//grid.add(rect, 0, 0);
+		//button3.setText("oh");
+		
+		
+		
+	
 	}
 }
 
