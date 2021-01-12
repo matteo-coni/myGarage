c2c50944f9d05872cf66876eb5d8f27a2171d8ce
Matteo Coni <57217715+matteo-coni@users.noreply.github.com>
2021-01-12 13:05:11 +0100

    proveview

--- src/logic/view/desktop/ControllerGrafHome.java
+++ src/logic/view/desktop/ControllerGrafHome.java
@@ -10,6 +10,7 @@ import javafx.scene.layout.*;
 import javafx.fxml.FXML;
 import javafx.scene.shape.*;
 import javafx.scene.paint.Color;
+import javafx.scene.text.*;
 
 import logic.view.desktop.ViewOwnerSaveVehicle;
 
@@ -18,11 +19,14 @@ public class ControllerGrafHome /* extends Application*/ {
 	
 	@FXML
 	Button button;
-	Button button1;
-	Button button2;
+	@FXML
 	Button button3;
+	Button button2;
+	
 	Image imageVehicle;
 	GridPane grid;
+	@FXML
+	Text label1;
 	
 	@FXML
 	AnchorPane anchor;
@@ -34,7 +38,8 @@ public class ControllerGrafHome /* extends Application*/ {
 		//button.setText("oh");
 		System.out.println("ciaociao");
 		//grid.add(rect, 0, 0);
-		//button3.setText("oh");
+		button3.setText("oh");
+		label1.setText("essu");
 		
 		
 		
