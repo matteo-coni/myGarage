f22057501791d5df09a3ca9851e40a32b9a5ab25
Matteo Coni <57217715+matteo-coni@users.noreply.github.com>
2021-02-01 19:16:43 +0100

    bookmech

--- src/logic/view/desktop/controllerview/ControllerGrafBookMech.java
+++ src/logic/view/desktop/controllerview/ControllerGrafBookMech.java
@@ -1,12 +1,16 @@
 package logic.view.desktop.controllerview;
 
 import java.io.IOException;
+import java.time.LocalDate;
 
 import javafx.fxml.FXML;
 import javafx.scene.image.ImageView;
 import logic.view.desktop.factory.viewfactory.FactoryView;
 import logic.view.desktop.factory.viewfactory.TypeView;
 import logic.view.desktop.view.View;
+import javafx.scene.control.DatePicker;
+
+import java.util.*;
 
 public class ControllerGrafBookMech {
 
@@ -14,6 +18,9 @@ public class ControllerGrafBookMech {
 	ImageView homeImage;
 	@FXML
 	ImageView insertImage;
+	
+	@FXML
+	DatePicker datePick;
 	
 	@FXML
 	public void back() throws IOException{
@@ -41,5 +48,13 @@ public class ControllerGrafBookMech {
 			//dummy
 		}
 		
+	}
+	
+	@FXML
+	public void confirmBooking() {
+		/*String data = datePick.getAccessibleText();
+		System.out.println(data);
+		LocalDate datap = datePick.getValue();
+		System.out.println(datap);*/
 	}
 }
