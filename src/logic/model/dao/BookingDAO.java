aabcfa5b1d4d79b2c1fcafa9ebe98900ef9c8366
Matteo Coni <57217715+matteo-coni@users.noreply.github.com>
2021-02-09 18:16:38 +0100

    ok

--- src/logic/model/dao/BookingDAO.java
+++ src/logic/model/dao/BookingDAO.java
@@ -6,7 +6,11 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.text.DateFormat;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 import java.util.Locale;
 
 import logic.model.*;
@@ -16,7 +20,7 @@ public class BookingDAO {
 	//Stringa di connessione al db con nome e porta
 	static final String URL = "jdbc:mysql://localhost:3306/mydb";
 	// Stringa driver per il db
-	static final String DRIVER = "com.mysql.jdbc.Driver";
+	static final String DRIVER = "com.mysql.cj.jdbc.Driver";
 	static final String USERNAMEDB = "root";
 	static final String PASSWORDDB = "admin";
 		
@@ -31,7 +35,7 @@ public class BookingDAO {
 		
 		
 		try {
-            
+			Class.forName(DRIVER);
             // Otteniamo una connessione con username e password
             con = DriverManager.getConnection (URL , USERNAMEDB, PASSWORDDB);
             
@@ -55,7 +59,10 @@ public class BookingDAO {
 		   } catch (SQLException e) {
 	            e.printStackTrace();
 	            
-	        } finally {
+	        } catch (ClassNotFoundException e) {
+			
+			e.printStackTrace();
+		} finally {
 	       
 	            if (stm != null) {
 	                try {
@@ -77,6 +84,97 @@ public class BookingDAO {
 	        }
 				
     }		
+	
+	public List<Booking> findBooking(String nomeOfficina){
+		
+		List<Booking> listBooking = new ArrayList<>();
+		
+		ResultSet rs= null;
+		Connection con = null;
+		Statement stm = null;
+		
+		try {
+			Class.forName(DRIVER);
+            // Otteniamo una connessione con username e password
+            con = DriverManager.getConnection (URL , USERNAMEDB, PASSWORDDB);
+            
+            // Creiamo un oggetto Statement per interrogare il db
+             stm = con.createStatement (ResultSet.TYPE_SCROLL_INSENSITIVE,
+                    ResultSet.CONCUR_READ_ONLY);
+            
+            // query ---> immagazziniamone i risultati	in result set	INSERIRE LA GIUSTA QUERY PER CERCARE NEL DB
+            
+            String query = "SELECT * FROM Booking WHERE Officina = '" + nomeOfficina + "' ORDER BY Data ASC;"; //Scrivere query per cercare by city
+            System.out.println(query); //prova stringa query
+            
+            rs = stm.executeQuery(query);
+            
+            rs.first();
+           
+            do{
+                
+                String username = rs.getString("Username");
+                
+                //String Officina = rs.getString("");
+                String problemi = rs.getString("Problemi");
+                String data = rs.getString("Data");
+                Date date=new SimpleDateFormat("dd/MM/yyyy").parse(data);
+                
+                Boolean confermata = rs.getBoolean("Confermata");
+                String veicolo = rs.getString("Veicolo");
+                
+                Booking booking = new Booking();
+                booking.setNameGarage(nomeOfficina);
+                booking.setUsername(username);
+                booking.setProblems(problemi);
+                booking.setDateBook(date);
+                booking.setSelVehicle(veicolo);
+                booking.setBookingConfirm(confermata);
+
+                listBooking.add(booking);
+
+            }while(rs.next());
+               
+        } catch (SQLException se) {
+            se.printStackTrace();
+            
+        } catch (ClassNotFoundException e) {
+			
+			e.printStackTrace();
+		} catch (ParseException e) {
+			e.printStackTrace();
+			
+		} finally {
+            
+        	if (rs != null) {
+                try {
+                    rs.close();
+                } catch (SQLException sqle) {
+                    sqle.printStackTrace();
+                }
+            }
+            rs = null;
+            if (stm != null) {
+                try {
+                    stm.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+            stm = null;
+            if (con != null) {
+                try {
+                    con.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+                con = null;
+            }
+        }
+		
+		return listBooking; 
+		
+	}
 	
 
 }
