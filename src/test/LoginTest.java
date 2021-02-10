9cc44f9490c719197e9ea267785e9ca37b161369
Matteo Coni <57217715+matteo-coni@users.noreply.github.com>
2021-02-10 15:46:19 +0100

    test

--- src/test/LoginTest.java
+++ src/test/LoginTest.java
@@ -4,11 +4,31 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import logic.bean.LoginBean;
+import logic.control.ControllerLogin;
+
 public class LoginTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void CheckLogin() {
+		
+		ControllerLogin controlLogin = new ControllerLogin();
+		LoginBean loginBean = new LoginBean("admin","admin");
+		
+		Boolean check = controlLogin.isValidate(loginBean);
+		
+		assertEquals(true,check);
+		
+	}
+	
+	@Test
+	public void CheckLoginMech() {
+		ControllerLogin controlLoginMech = new ControllerLogin();
+		LoginBean loginBeanMech = new LoginBean("mechanic","mechanic");
+		
+		Boolean checkMech = controlLoginMech.isValidateMech(loginBeanMech);
+		
+		assertEquals(true,checkMech);
 	}
 
 }
