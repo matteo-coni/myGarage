506fed94c4d1756dcb93bfd78a92072e05330da0
Matteo Coni <57217715+matteo-coni@users.noreply.github.com>
2021-02-11 19:29:02 +0100

    ok

---
+++ src/logic/exception/EmptyDatePickException.java
@@ -0,0 +1,11 @@
+package logic.exception;
+
+public class EmptyDatePickException extends Exception {
+
+	
+	private static final long serialVersionUID = -1087345248663858448L;
+
+	public EmptyDatePickException (String message){
+		super(message);
+	}
+}
